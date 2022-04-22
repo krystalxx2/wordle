a2fe348ebaeaddf9442754223710d4efb8528502
Krystal Rodriguez <krystal@krystalsmacbook.myfiosgateway.com>
2022-04-21 21:29:17 -0400

    first commit

---
+++ src/components/GameOver.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+
+function GameOver() {
+  const {
+    board,
+    setBoard,
+    currAttempt,
+    gameOver,
+    onSelectLetter,
+    correctWord,
+    onDelete,
+  } = useContext(AppContext);
+  return (
+    <div className="gameOver">
+      <h3>
+        {gameOver.guessedWord
+          ? "You Correctly Guessed the Wordle"
+          : "If you got the Wordle Correct you're thee ONE, If you got it wrong go and read a book or something smh"
+          
+        }
+      </h3>
+      <h1>Correct Word: {correctWord}</h1>
+      {gameOver.guessedWord && (
+        <h3>You guessed in {currAttempt.attempt} attempts</h3>
+      )}
+    </div>
+  );
+}
+
+export default GameOver;
